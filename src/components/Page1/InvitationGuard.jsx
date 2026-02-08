34084b5c998e5609c6b2bfa3eae9e4e4a0eafc26
Masamune Date <haris.solo68@gmail.com>
2026-02-09 06:32:50 +0700

    change invitation cover name couple size

--- src/components/Page1/InvitationGuard.jsx
+++ src/components/Page1/InvitationGuard.jsx
@@ -5,7 +5,7 @@ export default function InvitationGuard({ onOpen }) {
 
   return (
     <div
-      className="fixed inset-0 bg-cover bg-center bg-no-repeat"
+      className="fixed inset-0 bg-cover bg-no-repeat bg-center"
       style={{ backgroundImage: "url('/assets/bg-cover.png')" }}
     >
 
@@ -18,30 +18,30 @@ export default function InvitationGuard({ onOpen }) {
           Wedding Invitation
         </p>
 
-  {/* 2. Suci - Left-Top */}
-{/* Moved UP from 28% to 20% */}
-<h1 className="absolute top-[25%] left-[10%] font-loveLight text-[90px] sm:text-7xl text-white leading-none z-20">
-  Suci
-</h1>
+        {/* 2. Suci - Left-Top */}
+        {/* Moved UP from 28% to 20% */}
+        <h1 className="absolute top-[25%] left-[10%] md:left-[25%] font-loveLight text-[90px] sm:text-7xl text-white leading-none z-20">
+          Suci
+        </h1>
 
-{/* 3. & - Center */}
-{/* Moved UP from 50% to 32% */}
-<p className="absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 font-loveLight text-8xl sm:text-5xl text-white z-20">
-  &
-</p>
+        {/* 3. & - Center */}
+        {/* Moved UP from 50% to 32% */}
+        <p className="absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 font-loveLight text-8xl sm:text-5xl text-white z-20">
+          &
+        </p>
 
-{/* 4. Seky - Right-Bottom */}
-{/* Moved UP from 58% to 45% */}
-<h1 className="absolute top-[45%] right-[10%] font-loveLight text-[90px] sm:text-7xl text-white leading-none z-20">
-  Seky
-</h1>
+        {/* 4. Seky - Right-Bottom */}
+        {/* Moved UP from 58% to 45% */}
+        <h1 className="absolute top-[45%] right-[10%] md:right-[25%] font-loveLight text-[90px] sm:text-7xl text-white leading-none z-20">
+          Seky
+        </h1>
 
         {/* 5. to - Bottom (above button) */}
         <p className="absolute bottom-44 left-1/2 -translate-x-1/2 font-dmSans text-xl whitespace-nowrap">
           {guestName ? 'Kepada:' : 'To :'}
         </p>
         <p className="absolute bottom-36 left-1/2 -translate-x-1/2 font-dmSans text-xl font-bold whitespace-nowrap">
-          {guestName || 'Haris dan Rezkita'}
+          {guestName || 'Tamu Undangan'}
         </p>
 
         {/* 6. Open Invitation Button - Bottom Center */}
